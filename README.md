b029652ec68f1de87bec2b695d00fa0fbee17eef
Eric <ericli21@bu.edu>
2018-12-01 13:30:39 -0500

    Added comments and updated readme

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
-# EC601 Mini Project 1 Submission - Eric Li
-This is my library/API for Mini Project 1. The purpose of this set of code is to pull image data from a twitter feed into the current directory, describe these images using Google Vision labels (stored into a text file in the directory), and combining the images into a video through FFMPEG. MiniProject1.py is a main script that shows how to use all the functions in each library.
+# EC601 Mini Project 3 Submission - Eric Li
+This is my library/API for Mini Project 3. The purpose of this set of code is to store image data that was pulled from a twitter feed into the current directory, as well as the Google Vision labels (stored into a text file in the directory) used to analyze these images. MiniProject3.py is a main script building off of the original script (MiniProject1.py) and shows how to use all the database functions in each library.
 
 ## Setup
 In order to run the main script, open your terminal (I used git bash) and cd into your folder containing the files. In the bash, type: 
 ```
-python MiniProject1.py
+python MiniProject3.py
 ```
-Please note that you should put your own API keys in MiniProject1.py to run successfully. Make sure that your environment variable in bash include your python.exe and FFMPEG.exe files. Keep the folder clear of any images, text files or videos, as they will be replaced with new files everytime you run the main script. Please also note that you should never give out API keys to anyone/ on GitHub. 
+Please note that you should put your own API keys in MiniProject3.py to run successfully. Make sure that your environment variable in bash include your python.exe and FFMPEG.exe files. Keep the folder clear of any images, text files or videos, as they will be replaced with new files everytime you run the main script. Please also note that you should never give out API keys to anyone/ on GitHub. 
+
+For setting up the databases, you may need to download and set up environments for (MongoDB)[https://www.mongodb.com/download-center] and (MySQL)[https://dev.mysql.com/downloads/mysql/]. For MongoDB, I downloaded the MongoDB package as well as Mongo Compass to visualize the database. I also used the pymongo API to connect to localhost and set up an admin account. For MySQL, I downloaded the python connector to MySQL, as well as the MySQL community server, shell and workbench. I used the pymysql API for working on this part.
+
+For setting up admin and user privileges on MySQL, go to the MySQL shell as type:
+```
+\sql
+\connect root@localhost
+CREATE DATABASE TESTDB;
+GRANT ALL PRIVILEGES ON TESTDB.* TO 'host'@'localhost';
+```
 
 ## Additional Notes
-The max number of tweets that can be pulled is set to 50. This is to prevent going over the rate limit. When creating a video, there may be a strange delay in the beginning, showing nothing but a black screen. However, it will later show the images in order.
+The max number of tweets (and images) that can be pulled is set to 50. This is to prevent going over the rate limit. When creating a video, there may be a strange delay in the beginning, showing nothing but a black screen. However, it will later show the images in order.
+
+The MongoDB and MySQL functions were intended to be similar: to store all images and labels, search which users had a certain label, and common label and image statistic. However, since both are different at handling data (specific lists and lists of lists), some features are left out from MySQL. The MongoDB is able to store image data (varying byte string length) and varying sizes of arrays, whereas in MySQL these variables had to be worked around by repeating image numbers, users, and handles for every single label. 
+
+Additional directions to go into for this project would be to see how many times every user has used the system, as well as opening up images to show certain labels. 
 
 ## Sources
 1. https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/entities-object.html#media
@@ -17,3 +31,5 @@ The max number of tweets that can be pulled is set to 50. This is to prevent goi
 3. https://cloud.google.com/vision/docs/reference/rest/v1/images/annotate
 4. https://docs.python.org/3/library/subprocess.html
 5. https://ffmpeg.org/ffmpeg.html
+6. https://www.w3schools.com/python/python_mongodb_getstarted.asp
+7. https://www.tutorialspoint.com/python3/python_database_access.htm
--- databaseAPI.py
+++ databaseAPI.py
@@ -1,3 +1,5 @@
+#databaseAPI: Store tweet information (pictures, labels, and user information) in Mongo and MySQL databases
+
 import pymysql
 import pymongo
 import datetime
@@ -8,15 +10,20 @@ import base64
 import operator
 import collections
 
+#Create a mongo class for all MongoDB functions
 class MongoTweets:
+	#Log in to localhost and connect to the client
+	#Clearance is set as true as the sign_in is not necessary for this project
 	def __init__(self):
 		self.url = "mongodb://Testing:Testing@localhost:27017"
 		self.client = pymongo.MongoClient(self.url)
 		self.db = self.client["tweets"]
 		self.clearance = True
 
+	#Sign in to the database to gain proper access for reading and writing to the database
+	#If incorrect, there is an option to make an account
 	#Optional use: since it is on localhost, there may not be a need for passwords and this implementation
-	#Clearance defaults to True because of this
+	#Clearance defaults to True because of this (Set to False if using this)
 	def sign_in(self):
 		user_name = input("Enter your username: ")
 		pass_word = input("Enter your password: ")
@@ -36,36 +43,48 @@ class MongoTweets:
 		 		db.add_user(new_user_name, new_pass_word, roles=[{'role':'readWrite','db':'tweets'}])
 		 		return(sign_in()) 
 
+	#Add a tweet by searching the labels file AFTER analyzing (and pulling) the images using google vision
+	#Read each line in labels.txt, and convert all images to byte strings and store both as seperate arrays
+	#Store total number of images, the twitter handle, the date, and the user who ran the session
 	def add_tweet(self, user_name, handle):
 		if self.clearance == True:
 			images = []
 			labels = []
-			with open("labels.txt", 'r') as ins:
-				for line in ins:
-					sep = line.split()
-					if sep != []:
-						labels.append(sep[3:])
-			for image_name in os.listdir('.'):
-					if image_name.endswith('.jpg'):
-						with io.open(image_name, 'rb') as image_file:
-							content_string = base64.b64encode(image_file.read())
-							images.append(content_string)
-			entry = {
-				'user': user_name,
-				'handle': handle,
-				'imagecount': len(images),
-				'images': images,
-				'labels': labels,
-				'time': datetime.datetime.utcnow()
-			}
-			entry_id = self.db["tweets"].insert_one(entry)
+			try:
+				with open("labels.txt", 'r') as ins:
+					for line in ins:
+						sep = line.split()
+						if sep != []:
+							labels.append(sep[3:])
+				for image_name in os.listdir('.'):
+						if image_name.endswith('.jpg'):
+							with io.open(image_name, 'rb') as image_file:
+								content_string = base64.b64encode(image_file.read())
+								images.append(content_string)
+				entry = {
+					'user': user_name,
+					'handle': handle,
+					'imagecount': len(images),
+					'images': images,
+					'labels': labels,
+					'time': datetime.datetime.utcnow()
+				}
+				entry_id = self.db["tweets"].insert_one(entry)
+			except:
+				print("Please make labels before using this function")
 
+	#Read into an array of arrays (each picture has an array of labels) to look for a specific label
+	#If there is a hit, print which user had this label in their session
 	def find_label(self, label):
 		doc = self.db['tweets'].find({'labels': {'$elemMatch': {'$elemMatch': {'$in': [label]}}}})
 		print(label, "found in entries:")
 		for x in doc:
 			print('User:', x['user'], '	Handle used:', x['handle'], '	Time:', x['time'])
 
+	#Search for all entries with labels (should be all of them)
+	#Combine all labels from every images into one big list, 
+	#Use a Counter dictionary to search for the top three common labels
+	#Count the total amount of images, and the average image amount per session
 	def all_stats(self):
 		doc = self.db['tweets'].find({"labels": {'$exists': True}})
 		total_list = []
@@ -79,7 +98,6 @@ class MongoTweets:
 		print("%s: %d hits" % (max_three_labels[0], counts[max_three_labels[0]]))
 		print("%s: %d hits" % (max_three_labels[1], counts[max_three_labels[1]]))
 		print("%s: %d hits" % (max_three_labels[2], counts[max_three_labels[2]]))
-		
 		doc = self.db['tweets'].find({"imagecount": {'$exists': True}})
 		total_images = 0
 		per_entry = 0
@@ -89,7 +107,12 @@ class MongoTweets:
 		print("Total images in database:", total_images)
 		print("Average images per feed:", total_images/per_entry)
 
+#Create a MySQL class for all MySQL functions
 class MySQLTweets:
+	#Log in to localhost and database, obtain a cursor
+	#May need to set up an admin and password as well as a database via MySQL shell
+	#If there isn't a database, make one
+	#Database features: Session user, Twitter handle of images, date/time, image number, and the labels
 	def __init__(self, username, password, database):
 		self.db = pymysql.connect("localhost", username, password, database)
 		self.cursor = self.db.cursor()
@@ -104,6 +127,8 @@ class MySQLTweets:
 		except Exception as e:
 			print(e)
 
+	#Search the labels.txt (again, make sure you have use the google vision function first!) for labels
+	#For every label, store the corresponding image number and twitter handle, as well as the session user and date/time
 	def add_tweet(self, user_name, handle):
 		labels = []
 		with open("labels.txt", 'r') as ins:
@@ -120,7 +145,9 @@ class MySQLTweets:
 						except Exception as e:
 							print(e)
 							self.db.rollback()
-		
+	
+	#To find a specific label that a user has used, pull all users that has used the label
+	#Then print out which users have used the image
 	def find_label(self, label):
 		sql = "SELECT USER FROM TWEETS \
 			WHERE LABEL = %s"
@@ -135,6 +162,11 @@ class MySQLTweets:
 		except Exception as e:
 			print(e)
 
+	#Grab all labels from the database to find the three most commonly used tags
+	#Like with MongoDB, find the number of images per session 
+	#Since the database is stored sequentially (Session 1: Image 0, Image 1 .. Image 40 -> Session 2: Image 0, Image 1..)
+	#We can find the max number of images per session
+	#Calculate total images and average number of images per feed
 	def all_stats(self):
 		sql = "SELECT LABEL, IMAGENUMBER FROM TWEETS"
 		try:
@@ -162,6 +194,7 @@ class MySQLTweets:
 
 		except Exception as e:
 			print(e)
-		
+	
+	#When done, shut off the connection
 	def shut(self):
 		self.db.close()
